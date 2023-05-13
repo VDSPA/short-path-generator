2fe9a193b0fcff659b00acc117b9084cad3f36e6
jyfeng <1799587240@qq.com>
2023-05-13 17:04:04 +0800

    feat(algorithm): add getAllDistances func
    chore(algorithm): reset state before running

--- src/main/java/team/smd/vdsp/utils/Floyd.java
+++ src/main/java/team/smd/vdsp/utils/Floyd.java
@@ -68,9 +68,18 @@ public class Floyd extends ShortestPath {
 	}
 
 	/**
+	 * get all shortest distances
+	 */
+	public int[] getAllDis() {
+		return shortestDis;
+	}
+
+	/**
 	 * Get the shortest path through multiple loops
 	 */
 	public void shortest() {
+		this.stepQueue.clear();
+		this.allPath.clear();
 		for (int k = 0; k < vSize; k++) {
 			for (int i = 0; i < vSize; i++) {
 				for (int j = 0; j < vSize; j++) {
--- src/main/java/team/smd/vdsp/utils/ShortestPath.java
+++ src/main/java/team/smd/vdsp/utils/ShortestPath.java
@@ -9,98 +9,100 @@ import team.smd.vdsp.models.Step;
  * includes paths, traversing steps
  */
 public abstract class ShortestPath {
-    /** matrix */
-    int adjMatrix[][];
+	/** matrix */
+	int adjMatrix[][];
 
-    /** start point id */
-    int start;
+	/** start point id */
+	int start;
 
-    /** numbers of vertexes */
-    int vSize;
+	/** numbers of vertexes */
+	int vSize;
 
-    /** steps queue */
-    LinkedList<Step> stepQueue = new LinkedList<Step>();
+	/** steps queue */
+	LinkedList<Step> stepQueue = new LinkedList<Step>();
 
-    /** all shortest path */
-    ArrayList<String> allPath = new ArrayList<String>();
+	/** all shortest path */
+	ArrayList<String> allPath = new ArrayList<String>();
 
-    public ShortestPath() {
-        this.adjMatrix = new int[][] { {} };
-        this.start = 0;
-        this.vSize = adjMatrix.length;
-    }
+	public ShortestPath() {
+		this.adjMatrix = new int[][] { {} };
+		this.start = 0;
+		this.vSize = adjMatrix.length;
+	}
 
-    public ShortestPath(int adjMatrix[][], int start) {
-        this.adjMatrix = adjMatrix;
-        this.start = start;
-        this.vSize = adjMatrix.length;
+	public ShortestPath(int adjMatrix[][], int start) {
+		this.adjMatrix = adjMatrix;
+		this.start = start;
+		this.vSize = adjMatrix.length;
 
-    }
+	}
 
-    /**
+	/**
 	 * Run specific algorithm to find shortest path.
 	 * Stores result in member variables
-     */
-    public abstract void shortest();
+	 */
+	public abstract void shortest();
 
-    /**
-     * get all shortest Path
-     * 
-     * @return all Path
-     */
-    public ArrayList<String> getAllPaths() {
-        return this.allPath;
-    }
+	/**
+	 * get all shortest Path
+	 * 
+	 * @return all Path
+	 */
+	public ArrayList<String> getAllPaths() {
+		return this.allPath;
+	}
 
-    /**
-     * get all steps
-     * 
-     * @return step queue
-     */
-    public LinkedList<Step> getAllSteps() {
-        return this.stepQueue;
-    }
+	/**
+	 * get all steps
+	 * 
+	 * @return step queue
+	 */
+	public LinkedList<Step> getAllSteps() {
+		return this.stepQueue;
+	}
 
-    /**
-     * get vertex size
-     * 
-     * @return vertex size
-     */
-    public int getVSize() {
-        return vSize;
-    }
+	public abstract int[] getAllDis();
 
-    /**
-     * show adjMatrix
-     * 
-     * @return matrix
-     */
-    public String toMatrix() {
-        String matrix = "";
-        for (int i = 0; i < adjMatrix.length; i++) {
-            for (int j = 0; j < adjMatrix[i].length; j++) {
-                if (adjMatrix[i][j] == Integer.MAX_VALUE) {
-                    matrix += String.format("%5s", "MAX");
-                } else {
-                    matrix += String.format("%5s ", adjMatrix[i][j]);
-                }
-            }
-            matrix += "\n";
-        }
-        return matrix;
-    }
+	/**
+	 * get vertex size
+	 * 
+	 * @return vertex size
+	 */
+	public int getVSize() {
+		return vSize;
+	}
+
+	/**
+	 * show adjMatrix
+	 * 
+	 * @return matrix
+	 */
+	public String toMatrix() {
+		String matrix = "";
+		for (int i = 0; i < adjMatrix.length; i++) {
+			for (int j = 0; j < adjMatrix[i].length; j++) {
+				if (adjMatrix[i][j] == Integer.MAX_VALUE) {
+					matrix += String.format("%5s", "MAX");
+				} else {
+					matrix += String.format("%5s ", adjMatrix[i][j]);
+				}
+			}
+			matrix += "\n";
+		}
+		return matrix;
+	}
 
 	@Override
-    public String toString() {
-        String info = "";
-        // String temp=this.allPath.toString();
-        for (int i = 0; i < allPath.size(); i++) {
-            info += allPath.get(i);
-            // temp.add(allPath.get(i));
-        }
-        for (int i = 0; i < stepQueue.size(); i++) {
-            info += stepQueue.get(i) + "\n";
-        }
-        return info;
-    }
+	public String toString() {
+		String info = "";
+		// String temp=this.allPath.toString();
+		for (int i = 0; i < allPath.size(); i++) {
+			info += allPath.get(i);
+			// temp.add(allPath.get(i));
+		}
+		for (int i = 0; i < stepQueue.size(); i++) {
+			info += stepQueue.get(i) + "\n";
+		}
+		return info;
+	}
 }
